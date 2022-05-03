7c923da03de04c112c0e27a690b1dffd31413b6d
Matheus Filype <matheus.campos@gcbinvestimentos.com>
2022-05-03 12:07:09 -0300

    feat: project init

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Widget } from "./components/Widget";
+
+function App() {
+  return <Widget />;
+}
+
+export default App;
